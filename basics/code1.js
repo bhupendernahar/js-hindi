98013fef8f4110760952c8090da612ca4f74ec95
bhupendernahar <133181066+bhupendernahar@users.noreply.github.com>
2025-06-22 04:54:57 +0000

    basic variable

---
+++ basics/code1.js
@@ -0,0 +1,9 @@
+console.log("bhupender");
+
+const accountId=123
+let accountEmail="bhupender@gmail.com"
+var accountPassward ="873687"
+
+accountcity = "bangaluru"
+
+console.table([accountEmail , accountId , accountPassward , accountcity])
