24c9e4a8964f97dadbf0730086c0c1d02d30ebbd
bhupendernahar <133181066+bhupendernahar@users.noreply.github.com>
2025-06-25 09:21:58 +0000

    Add initial JavaScript files for basic operations and comparisons

---
+++ basics/02_code2.js
@@ -0,0 +1,7 @@
+const age=2
+let num =34
+let name= "sg"
+let rain = false
+
+console.log(num)
+console.log(age);
---
+++ basics/03_convertionOperation.js
@@ -0,0 +1,37 @@
+let age = 33
+
+//console.log(age);
+//console.log((age));
+//................
+let isLoggedIn = 44 ;
+let stringIsValue = String(isLoggedIn);
+
+//console.log(stringIsValue);
+//console.log(typeof stringIsValue);
+//..................
+let someNumber = "345"
+let noIsThat = Number(someNumber)
+
+//console.log(noIsThat)
+//console.log(typeof noIsThat)
+//...................
+let noValueIs = "Bhupender";
+
+let valBooleanIs = Boolean(noValueIs)
+
+//console.log(valBooleanIs)
+//console.log(typeof valBooleanIs)
+//....................
+
+let sCounut=100
+++sCounut;
+//sCounut++;
+//console.log(sCounut);
+
+
+let hr="hello" , hr1=" Bhupender";
+pr= hr + hr1;
+//console.log(pr)
+
+console.log(2 + 3 + "1")
+console.log(" 2 " + 3 + 1)
